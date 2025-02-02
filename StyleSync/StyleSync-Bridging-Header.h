5f741270c408f749034309122e77a9e62843450c
Biplove GC <gcbiplove123@gmail.com>
2025-02-02 12:58:13 -0800

    Initial Commit

---
+++ StyleSync/StyleSync-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  StyleSync-Bridging-Header.h
+//  StyleSync
+//
+//  Created by Biplove on 2/2/25.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
---
+++ StyleSyncTests/StyleSyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  StyleSyncTests.swift
+//  StyleSyncTests
+//
+//  Created by Biplove on 2/2/25.
+//
+
+import Testing
+@testable import StyleSync
+
+struct StyleSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
